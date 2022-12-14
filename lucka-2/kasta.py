356753c39ee6b1d8b00ab8d8ff6b339d07ecd236
Mikael Olshansky <mikael.olshansky@gmail.com>
2022-12-14 20:37:41 +0100

    Lucka 2: Kastar lussekatter

---
+++ lucka-2/kasta.py
@@ -0,0 +1,12 @@
+print('Here is the answer to the second door in the Code Calendar 2022, Mikael!')
+
+diff = 0
+distance = 0
+sum = 0
+
+while distance < 100:
+    sum += distance
+    diff += 1
+    distance += diff
+
+print(f'The sum is: {sum}')
